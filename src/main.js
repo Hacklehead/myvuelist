4958a5b226f3b0589511c9ecff7a40e3e62f36ae
Hacklehead <51390936+Hacklehead@users.noreply.github.com>
2019-08-26 20:45:47 -0400

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -1,12 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueResource from 'vue-resource'
+import VueRouter from 'vue-router'
 import router from './router'
+//  import Routes from './routes'
 import store from './store'
 
 Vue.config.productionTip = false
 
+// Use packages
+Vue.use(VueResource)
+Vue.use(VueRouter)
+
+// Register routes
+//
+// const myrouter = new VueRouter({
+//   routes: Routes,
+//   mode: 'history'
+// })
+
 new Vue({
   router,
   store,
+  VueResource,
   render: h => h(App)
 }).$mount('#app')
